57eba346574877243d93261a222d8ffbca39c2a3
Vladislav Mamon <norskeld@gmail.com>
2023-10-10 11:53:33 +0300

    Add basic parsers for literals

--- src/App/App.fs
+++ src/App/App.fs
@@ -1,16 +1,67 @@
-﻿open Attolog.Parser
+﻿open System
+
+open Attolog
+open Attolog.Parser
+open Attolog.Prelude
 
 let execute p input =
   let result = run p input
   let output = ParserResult.toString result
 
   printfn "%A" output
-  printfn "---------"
 
-let helloWorld =
-  pstring "Hello" .>> pwhitespace .>>. pstring "World"
-  |>> fun (first, second) -> $"{first}! {second}?!"
+// ---------------------------------------------------------------------------------------------------------------------
+// Terminals:
 
-execute helloWorld "Hello World"
-execute helloWorld "Hello    World"
-execute helloWorld "Hello        World"
+let pAssert = pstring ":-"
+let pQuery = pstring "?-"
+let pTrue = pstring "true"
+let pFalse = pstring "false"
+let pLeftParen = pstring "("
+let pRightParen = pstring ")"
+let pComma = pstring ","
+let pPeriod = pstring "."
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Non-terminals:
+
+let pConstant =
+  plowercase .>>. many (plowercase <|> puppercase <|> pdigit <|> pchar '_')
+  |>> (fun (first, rest) -> charsToString (first :: rest))
+
+let pVariable =
+  puppercase .>>. many (plowercase <|> puppercase <|> pdigit <|> pchar '_')
+  |>> (fun (first, rest) -> charsToString (first :: rest))
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Refs:
+
+let pArgs, pArgsRef = createForwardedParser<list<Syntax.Term>> ()
+let pLiteral, pLiteralRef = createForwardedParser<Syntax.Term> ()
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Definitions:
+
+do pArgsRef :=
+  let pArgsSingle = pLiteral |>> (fun literal -> [ literal ]) in
+  let pArgsMultiple = pLiteral .>> pComma .>>. pArgs |>> (fun (literal, args) -> literal :: args) in
+  pArgsMultiple <|> pArgsSingle
+
+do
+  pLiteralRef
+  := let pConst = pConstant |>> Syntax.Const in
+     let pVar = pVariable |>> (fun var -> Syntax.Var(var, 0)) in
+
+     let pApp =
+       pConstant .>>. between pLeftParen pArgs pRightParen
+       |>> (fun (constant, args) -> Syntax.App(constant, args)) in
+
+     pApp <|> pConst <|> pVar
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Examples:
+
+execute pLiteral "hello"
+execute pLiteral "HelloWorld"
+execute pLiteral "hello(arg)"
+execute pLiteral "hello(xarg,yarg)"
