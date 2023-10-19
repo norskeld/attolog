cdff5bb835ee8a9083e1325446ba90752dc5cc62
Vladislav Mamon <norskeld@gmail.com>
2023-10-19 11:28:28 +0300

    Co-locate syntax-related functions, add string conversions

--- Attolog/Core/Syntax.fs
+++ Attolog/Core/Syntax.fs
@@ -12,20 +12,20 @@ type Term =
   | Var of Variable
   /// Constant `a`, `b`, `c`, ...
   | Const of Constant
-  /// Compound term `f(t_1,...,t_n)`
+  /// Compound term `f(t1, ..., tn)`
   | App of Constant * list<Term>
 
-/// Atomic proposition `p(t_1, ..., t_n)`
+/// Atomic proposition `p(t1, ..., tn)`
 type Atom = Constant * list<Term>
 
-/// A conjunction of atomic propositions `p_1, ..., p_n`. The empty list represents `true`.
+/// A conjunction of atomic propositions `p1, ..., pn`. The empty list represents `true`.
 type Clause = list<Atom>
 
-/// An assertion `(a,b_1,...,b_n)` is a Horn formula `b_1 & ... & b_n => a`.
+/// An assertion `(a, b1, ..., bn)` is a Horn formula `b1 & ... & bn => a`.
 type Assertion = Atom * Clause
 
 /// An environment represents the current values of variables.
-type Env = list<Variable * Term>
+type Env = { values: list<Variable * Term> }
 
 /// A database is a list of assertions. It represents the current program.
 type Database = list<Assertion>
@@ -38,3 +38,50 @@ type Command =
   | Query of Clause
   /// Comment. Technically not a command or expression, I'm just being lazy here.
   | Comment of string
+
+/// Returns the value of variable instance `x` in environment `env`.
+let lookup (env: Env) (var: Variable) : Term =
+  try
+    env.values |> List.find (fst >> ((=) var)) |> snd
+  with _ ->
+    Var(var)
+
+/// Substitutes in term `t` values for variables, as specified by the `env`. It substitutes repeatedly until terms
+/// stops changing, so this is not the usual kind of substitution. It is what we need during unification.
+let rec substitute (env: Env) (term: Term) : Term =
+  match term with
+  | Var(var) as term ->
+    let found = lookup env var
+
+    if term = found then found else substitute env found
+  | Const(_) as term -> term
+  | App(constant, terms) -> App(constant, List.map (substitute env) terms)
+
+/// Checks if variable instance `x` appears in term `term`.
+let rec occurs (x: Variable) (term: Term) : bool =
+  match term with
+  | Var(var) -> var = x
+  | Const(_) -> false
+  | App(_, terms) -> List.exists (occurs x) terms
+
+type Term with
+
+  /// Returns a string representation of the term.
+  static member toString(term: Term) : string =
+    match term with
+    | Var(var, _) -> var
+    | Const(constant) -> constant
+    | App(constant, terms) -> constant + " " + String.concat " " (List.map Term.toString terms)
+
+type Env with
+
+  /// Returns a string representation of the environment. It only keeps instance variables at level 0, i.e. those that
+  /// appear in the toplevel query.
+  static member toString(env: Env) : string =
+    match List.filter (fun ((_, n), _) -> n = 0) env.values with
+    | [] -> "Yes"
+    | env' ->
+      let items =
+        List.map (fun ((x, _), e) -> x + " = " + Term.toString (substitute env e)) (List.rev env')
+
+      String.concat "\n" items
--- Attolog/Core/Unifier.fs
+++ Attolog/Core/Unifier.fs
@@ -5,31 +5,6 @@ open Attolog.Core.Syntax
 /// Raised when terms cannot be unified.
 exception NoUnify
 
-/// Returns the value of variable instance `x` in environment `env`.
-let lookup (env: Env) (var: Variable) : Term =
-  try
-    env |> List.find (fst >> ((=) var)) |> snd
-  with _ ->
-    Var(var)
-
-/// Substitutes in term `t` values for variables, as specified by the `env`. It substitutes repeatedly until terms
-/// stops changing, so this is not the usual kind of substitution. It is what we need during unification.
-let rec substitute (env: Env) (term: Term) : Term =
-  match term with
-  | Var(var) as term ->
-    let found = lookup env var
-
-    if term = found then found else substitute env found
-  | Const(_) as term -> term
-  | App(constant, terms) -> App(constant, List.map (substitute env) terms)
-
-/// Checks if variable instance `x` appears in term `term`.
-let rec occurs (x: Variable) (term: Term) : bool =
-  match term with
-  | Var(var) -> var = x
-  | Const(_) -> false
-  | App(_, terms) -> List.exists (occurs x) terms
-
 /// Tries to unify terms `t1` and `t2` in the current environment `env`. On success it returns the environment extended
 /// with the result of unification.
 ///
@@ -38,7 +13,11 @@ let rec unifyTerms (env: Env) (t1: Term) (t2: Term) : Env =
   match (substitute env t1, substitute env t2) with
   | t1, t2 when t1 = t2 -> env
   | Var(y), t
-  | t, Var(y) -> if occurs y t then raise NoUnify else (y, t) :: env
+  | t, Var(y) ->
+    if occurs y t then
+      raise NoUnify
+    else
+      { values = (y, t) :: env.values }
   | Const(_), _ -> raise NoUnify
   | App(c1, ts1), App(c2, ts2) when c1 = c2 -> unifyLists env ts1 ts2
   | App(_), _ -> raise NoUnify
