0e351f9c0eca5a0bb020ffd6b2ff84a081c3e2d5
Vladislav Mamon <norskeld@gmail.com>
2023-10-23 00:49:13 +0300

    Remove test program from main

--- Attolog/Main.fs
+++ Attolog/Main.fs
@@ -2,27 +2,7 @@ module Attolog.Main
 
 open System
 
-open Attolog.Prelude
 open Attolog.Core
-
-let program =
-  """
-  -- Define persons.
-  female(leia).
-  male(vader).
-  male(luke).
-  male(kylo).
-
-  -- Define relations.
-  child(luke, vader).
-  child(leia, vader).
-  child(kylo, leia).
-
-  son(X, Y) :- male(X), child(X, Y).
-  daughter(X, Y) :- female(X), child(X, Y).
-
-  grandchild(X, Z) :- child(X, Y), child(Y, Z).
-  """
 
 let introduction =
   let interrupt =
@@ -57,6 +37,6 @@ let main _ =
     | Ok commands ->
       for command in commands do
         Solver.solve command
-    | Error message -> printfn "%s" message
+    | Error message -> printfn $"{message}"
 
   0
